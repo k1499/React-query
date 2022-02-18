3107077729581dc35c4922cfaad76555357cb94d
KausikSS <>
2022-02-18 15:48:14 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import './App.css';
+ const queryClient = new QueryClient()
+ 
+ function App() {
+   return (
+     <QueryClientProvider client={queryClient}>
+       <App01 />
+     </QueryClientProvider>
+   )
+ }
+
+
+function App01() {
+  const { isLoading, error, data } = useQuery('repoData', () =>
+     fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+       res.json()
+     )
+   )
+ 
+   if (isLoading) return 'Loading...'
+ 
+   if (error) return 'An error has occurred: ' + error.message
+ 
+   return (
+     <div>
+       <h1>{data.name}</h1>
+       <p>{data.description}</p>
+       <strong>👀 {data.subscribers_count}</strong>{' '}
+       <strong>✨ {data.stargazers_count}</strong>{' '}
+       <strong>🍴 {data.forks_count}</strong>
+     </div>
+   )
+}
+
+export default App;
